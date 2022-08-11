cda970fab177108d256089c85e4f7ae6877092ad
양준수 <yangjs@markany.com>
2022-08-11 12:52:19 +0900

    Viewcontroller 생성

---
+++ RxMemoKxcoding/RxMemoKxcoding/ViewController/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  RxMemoKxcoding
+//
+//  Created by yangjs on 2022/08/11.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
---
+++ RxMemoKxcoding/RxMemoKxcoding/ViewController/MemoListViewcontroller.swift
@@ -0,0 +1,14 @@
+//
+//  MemoListViewcontroller.swift
+//  RxMemoKxcoding
+//
+//  Created by yangjs on 2022/08/11.
+//
+
+import UIKit
+
+class MemoListViewcontroller: UIViewController {
+
+   
+
+}
